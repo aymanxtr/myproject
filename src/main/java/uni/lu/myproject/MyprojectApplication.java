ceb48cfcb15800aafefd99c887612fdd4d99912b
aymanxtr <147649870+aymanxtr@users.noreply.github.com>
2024-05-26 13:27:23 +0200

    initialize project

---
+++ src/main/java/uni/lu/myproject/MyprojectApplication.java
@@ -0,0 +1,13 @@
+package uni.lu.myproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uni/lu/myproject/MyprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package uni.lu.myproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
